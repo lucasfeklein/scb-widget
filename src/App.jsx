8dca790747ba8f98db7fe0ae331c69cac81e5dfe
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-11 20:08:43 -0300

    indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { Icon } from '@iconify/react';
-import React, { useEffect, useRef, useState } from 'react';
-import { ThreeDots } from 'react-loader-spinner';
-import './index.css';
+import { Icon } from "@iconify/react";
+import React, { useEffect, useRef, useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
+import "./index.css";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [tooltipIsOpen, setTooltipIsOpen] = useState(true);
-  const [message, setMessage] = useState('');
-  const [chatHistory, setChatHistory] = useState([{ user: '', chatbot: '', typing: false }]);
+  const [message, setMessage] = useState("");
+  const [chatHistory, setChatHistory] = useState([
+    { user: "", chatbot: "", typing: false },
+  ]);
   const chatHistoryRef = useRef(null);
   const ws = useRef(null);
 
   const toggleChat = () => {
     setIsOpen(!isOpen);
-    setTooltipIsOpen(false)
+    setTooltipIsOpen(false);
   };
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
     e.target.style.height = "44px";
     e.target.style.height = e.target.scrollHeight + "px";
-    e.target.style.overflowY = e.target.scrollHeight > 120 ? "scroll" : "hidden";
+    e.target.style.overflowY =
+      e.target.scrollHeight > 120 ? "scroll" : "hidden";
   };
 
   const sendMessage = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (message.length > 0 && ws.current.readyState === WebSocket.OPEN) {
       ws.current.send(message);
-      setChatHistory(prevChatHistory => [...prevChatHistory, { user: message, chatbot: '', typing: true }]);
-      setMessage('');
+      setChatHistory((prevChatHistory) => [
+        ...prevChatHistory,
+        { user: message, chatbot: "", typing: true },
+      ]);
+      setMessage("");
     }
   };
 
@@ -37,20 +43,23 @@ function App() {
   };
 
   useEffect(() => {
-    ws.current = new WebSocket('ws://localhost:3000');
+    ws.current = new WebSocket("ws://localhost:3000");
     ws.current.onopen = () => {
-      console.log('Connected to WebSocket server.');
+      console.log("Connected to WebSocket server.");
     };
     ws.current.onmessage = (event) => {
-      setChatHistory(prevChatHistory => {
+      setChatHistory((prevChatHistory) => {
         const updatedHistory = [...prevChatHistory];
-        updatedHistory[updatedHistory.length - 1].chatbot = event.data.replace(/\n/g, '<br>');
+        updatedHistory[updatedHistory.length - 1].chatbot = event.data.replace(
+          /\n/g,
+          "<br>"
+        );
         updatedHistory[updatedHistory.length - 1].typing = false;
         return updatedHistory;
       });
     };
     ws.current.onclose = () => {
-      console.log('Disconnected from WebSocket server.');
+      console.log("Disconnected from WebSocket server.");
     };
     return () => {
       if (ws.current) {
@@ -67,28 +76,40 @@ function App() {
 
   return (
     <div className="fixed bottom-5 right-5">
-      {tooltipIsOpen && <div className='relative bg-blue-500 text-white rounded-lg p-4 mb-4 cursor-pointer' onClick={toggleChat}>
-        <p>Oi! Sou um chat IA.</p>
-        <p> Pergunte qualquer coisa.</p>
-        <div className='triangle'></div>
-      </div>}
-      <div className='flex justify-end'>
+      {tooltipIsOpen && (
+        <div
+          className="relative bg-blue-500 text-white rounded-lg p-4 mb-4 cursor-pointer"
+          onClick={toggleChat}
+        >
+          <p>Oi! Sou um chat IA.</p>
+          <p> Pergunte qualquer coisa.</p>
+          <div className="triangle"></div>
+        </div>
+      )}
+      <div className="flex justify-end">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-2xl py-2 px-4 rounded-full w-16 h-16"
           onClick={toggleChat}
         >
-          {isOpen ? 'X' : <i className="fa-solid fa-robot"></i>}
+          {isOpen ? "X" : <i className="fa-solid fa-robot"></i>}
         </button>
       </div>
       {isOpen && (
         <div className="flex flex-col justify-between absolute bottom-full right-0 bg-white border border-gray-300 rounded-t-lg shadow-lg mb-5 chatSize rounded-lg overflow-hidden">
-          <div className='border-b border-gray-300 p-4 mb-2 bg-blue-500 text-white'>
-            <p className='font-bold text-lg'>Dynamic Bot</p>
+          <div className="border-b border-gray-300 p-4 mb-2 bg-blue-500 text-white">
+            <p className="font-bold text-lg">Dynamic Bot</p>
           </div>
-          <div className="chatHistorySize overflow-y-auto mb-2 px-2" ref={chatHistoryRef}>
+          <div
+            className="chatHistorySize overflow-y-auto mb-2 px-2"
+            ref={chatHistoryRef}
+          >
             <div className="flex flex-row mb-3">
-              <span className='mr-2'><i className="fa-solid fa-robot"></i></span>
-              <p className='bg-gray-200 px-2 py-2 rounded-tr-lg rounded-bl-lg rounded-br-lg'>Olá! Como posso ajudar?</p>
+              <span className="mr-2">
+                <i className="fa-solid fa-robot"></i>
+              </span>
+              <p className="bg-gray-200 px-2 py-2 rounded-tr-lg rounded-bl-lg rounded-br-lg">
+                Olá! Como posso ajudar?
+              </p>
             </div>
             {chatHistory.map((message, index) => (
               <div key={index} className="text-gray-700">
@@ -96,20 +117,33 @@ function App() {
                   <div>
                     {message.user && (
                       <div className="flex flex-row justify-end mb-3">
-                        <p className='bg-blue-500 px-2 py-2 rounded-tl-lg rounded-bl-lg rounded-br-lg text-white'>{message.user}</p>
-                        <span className='ml-2'><i className="fa-solid fa-user"></i></span>
+                        <p className="bg-blue-500 px-2 py-2 rounded-tl-lg rounded-bl-lg rounded-br-lg text-white">
+                          {message.user}
+                        </p>
+                        <span className="ml-2">
+                          <i className="fa-solid fa-user"></i>
+                        </span>
                       </div>
                     )}
                     {message.typing && (
                       <div className="flex flex-row mb-3">
-                        <span className='mr-2'><i className="fa-solid fa-robot"></i></span>
-                        <p className='bg-gray-200 px-2 py-2 rounded-tr-lg rounded-bl-lg rounded-br-lg'><ThreeDots width={30} height={30} color='#808080' /></p>
+                        <span className="mr-2">
+                          <i className="fa-solid fa-robot"></i>
+                        </span>
+                        <p className="bg-gray-200 px-2 py-2 rounded-tr-lg rounded-bl-lg rounded-br-lg">
+                          <ThreeDots width={30} height={30} color="#808080" />
+                        </p>
                       </div>
                     )}
                     {message.chatbot && (
                       <div className="flex flex-row mb-3">
-                        <span className='mr-2'><i className="fa-solid fa-robot"></i></span>
-                        <p className='bg-gray-200 px-2 py-2 rounded-tr-lg rounded-bl-lg rounded-br-lg' dangerouslySetInnerHTML={{ __html: message.chatbot }}></p>
+                        <span className="mr-2">
+                          <i className="fa-solid fa-robot"></i>
+                        </span>
+                        <p
+                          className="bg-gray-200 px-2 py-2 rounded-tr-lg rounded-bl-lg rounded-br-lg"
+                          dangerouslySetInnerHTML={{ __html: message.chatbot }}
+                        ></p>
                       </div>
                     )}
                   </div>
@@ -127,14 +161,18 @@ function App() {
                 onChange={handleMessageChange}
                 onBlur={handleTextareaBlur}
                 onKeyDown={(e) => {
-                  if (e.key === 'Enter' && !e.shiftKey) {
+                  if (e.key === "Enter" && !e.shiftKey) {
                     sendMessage(e);
                   }
                 }}
-                style={{ height: "44px", maxHeight: '120px' }}
+                style={{ height: "44px", maxHeight: "120px" }}
+              ></textarea>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded text-xl absolute bottom-2 right-2"
+                type="submit"
               >
-              </textarea>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded text-xl absolute bottom-2 right-2" type="submit"><Icon icon="fe:paper-plane" /></button>
+                <Icon icon="fe:paper-plane" />
+              </button>
             </div>
           </form>
         </div>
