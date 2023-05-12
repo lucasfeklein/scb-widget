35bf4d06ecb8974f247d01bd3f528d9dfaa1a002
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-12 20:54:11 -0300

    styles.css moved to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 function renderMyWidget(element) {
   ReactDOM.createRoot(element).render(<App />);
 }
 
 (function () {
-  const container = document.createElement('div');
+  const container = document.createElement("div");
   document.body.appendChild(container);
   renderMyWidget(container);
 })();
