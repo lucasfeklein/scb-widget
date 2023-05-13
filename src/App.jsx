f7bb445fa2f2b54663d285a5715a2c4799462077
Rafael Muttoni <rafael@muttoni.com.br>
2023-05-13 13:07:51 +0700

    full refactor widget + style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,109 @@
 import React, { useState } from "react";
 import { FaRobot } from "react-icons/fa";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  z-index: 9999;
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+`;
+
+const Tooltip = styled.div`
+  position: relative;
+  background-color: #3b82f6;
+  color: white;
+  border-radius: 0.375rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  cursor: pointer;
+`;
+
+const ChatButton = styled.button`
+border: none;
+  background-color: #3b82f6;
+  color: white;
+  font-weight: bold;
+  font-size: 2rem;
+  padding: 0.5rem 1rem;
+  border-radius: 9999px;
+  width: 4rem;
+  height: 4rem;
+  transition: background-color 0.2s ease;
+
+  &:hover {
+    filter: brightness(1.1);
+  }
+
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const ChatFrame = styled.iframe`
+display: ${props => (props.isOpen ? "block" : "none")};
+  width: 100%;
+  height: 100%;
+  border: none;
+  position: fixed;
+  flex-direction: column;
+  justify-content: space-between;
+  box-shadow: rgba(150, 150, 150, 0.2) 0px 10px 30px 0px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px;
+  bottom: 5rem;
+  right: 1rem;
+  width: 448px;
+  height: 85vh;
+  max-height: 824px;
+  border-radius: 0.75rem;
+  display: ${props => (props.isOpen ? "block" : "none")};
+  z-index: 999999999;
+  overflow: hidden;
+  left: unset;
+  margin-bottom: 0.75rem;
+`;
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [tooltipIsOpen, setTooltipIsOpen] = useState(true);
 
- 
   const toggleChat = () => {
     setIsOpen(!isOpen);
     setTooltipIsOpen(false);
   };
 
   return (
-    <div className="fixed bottom-5 right-5 z-index-9999">
+    <>
+             <ChatFrame src={`http://localhost:3000/widget?hostname=${window.location.hostname}`} isOpen={isOpen} />
+
+             <Container>
       {tooltipIsOpen && (
-        <div
-          className="relative bg-blue-500 text-white rounded-lg p-4 mb-4 cursor-pointer"
-          onClick={toggleChat}
-        >
+        <Tooltip onClick={toggleChat}>
           <p>Oi! Sou um chat IA.</p>
           <p> Pergunte qualquer coisa.</p>
-          <div className="triangle"></div>
-        </div>
+        </Tooltip>
       )}
-      <div className="flex justify-end">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-2xl py-2 px-4 rounded-full w-16 h-16"
-          onClick={toggleChat}
-        >
-          {isOpen ? (
-            "X"
-          ) : (
-            <div className="flex align-center justify-center">
-              <FaRobot className="text-3xl" />
-            </div>
-          )}
-        </button>
-      </div>
-      {isOpen && (
-       <iframe src="http://localhost:3000/widget" />
-      )}
-    </div>
+      <ChatButton onClick={toggleChat}>
+        {isOpen ? (
+          "X"
+        ) : (
+          <div className="icon">
+            <FaRobot className="text-3xl" />
+          </div>
+        )}
+      </ChatButton>
+
+      
+
+
+    </Container>
+    </>
+   
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin(),],
+  plugins: [react(),],
   build: {
     lib: {
       entry: './src/main.jsx',
